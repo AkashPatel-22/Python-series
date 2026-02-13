42c2938b2b78ef5a3e8410c0441a75bde45c80f3
AkashPatel-22 <theaakashpatel22@gmail.com>
2026-02-13 10:42:21 +0530

    about strings and their syntax

---
+++ Day-5/STRINGS/string.py
@@ -0,0 +1,8 @@
+# about string - it is sequence of characters enclosed in qoutes.
+# immutable - one created cannot be changed
+
+# syntax
+str = "hello world"
+str2 = 'lets start'
+
+# written under double and single qoutes
